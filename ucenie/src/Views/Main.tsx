a60b2b7f9a3ac69ee0372e57ccd43c80edc7e4ee
FodoFofo <110025502+FodoFofo@users.noreply.github.com>
2023-07-01 08:50:56 +0000

    commit

---
+++ ucenie/src/Views/Main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <header>
+            <nav>
+                <ul>
+                    <li>React</li>
+                    <li>React<br />posuňte své webové aplikace na nový level</li>
+                    <li>React 2<br />pojďme vyvíjet pokročilejší aplikace</li>
+                    <li>React 3<br />posuňme své znalosti ještě dál</li>
+                    <li>...</li>
+                </ul>
+            </nav>
+        </header>
+            
+        Main
+    </div>
+  )
+}
+
+export default Main
