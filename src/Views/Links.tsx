08a222d8a1fa809f39dba7d9f7defe234248faeb
FodoFofo <110025502+FodoFofo@users.noreply.github.com>
2023-07-14 13:08:04 +0000

    doplnenie obsahu do Links a štýlovanie (čiastočne)

--- src/Views/Links.tsx
+++ src/Views/Links.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import './Links.scss'
 
 const Links = () => {
   return (
-    <div>Doplniť odkazy</div>
+    <div className='links' >
+      <h2>Odkazy</h2>
+      <a href="https://react.dev/learn/state-a-components-memory">Spraviť úlohy na spodku stránky</a>
+      <a href="https://zdrojak.cz/clanky/10-veci-jak-ai-pomaha-pri-programovani/ ">10 veci jak AI pomaha pri programovani/</a>
+      <a href="https://touchit.sk/youtube-na-pozadi-aj-bez-platenia-premium/479677 ">Youtube na pozadi aj bez platenia premium</a>
+    </div>
   )
 }
 
