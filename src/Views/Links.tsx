38478fd8252d81ca20a8a9ce0197171e387c9e49
FodoFofo <110025502+FodoFofo@users.noreply.github.com>
2023-08-01 18:27:55 +0000

    Doplnene linky a štýly

--- src/Views/Links.tsx
+++ src/Views/Links.tsx
@@ -3,10 +3,25 @@ import './Links.scss'
 const Links = () => {
   return (
     <div className='links' >
-      <h2>Odkazy</h2>
-      <a href="https://react.dev/learn/state-a-components-memory">Spraviť úlohy na spodku stránky</a>
-      <a href="https://zdrojak.cz/clanky/10-veci-jak-ai-pomaha-pri-programovani/ ">10 veci jak AI pomaha pri programovani/</a>
-      <a href="https://touchit.sk/youtube-na-pozadi-aj-bez-platenia-premium/479677 ">Youtube na pozadi aj bez platenia premium</a>
+      <p>odkazy podeliť podľa nejakých kategórii: nepozreté, štýlovanie, React, ....</p>
+      <h2>Odkazy:</h2>
+      <a href="https://dev.to/jeetvora331/throttling-in-javascript-easiest-explanation-1081" target='_blank' rel="noreferrer" >Throttling in JavaScript Easiest Explanation</a>
+      <a href="https://dev.to/abhixsh/a-comprehensive-beginners-guide-to-npm-simplifying-package-management-57l5" target='_blank' rel="noreferrer" >A Comprehensive Beginner's Guide to NPM: Simplifying Package Management</a>
+      <a href="https://www.freecodecamp.org/news/typescript-satisfies-operator/" target='_blank' rel="noreferrer" >How to Use the TypeScript satisfies Operator</a>
+      <a href="https://www.freecodecamp.org/news/typescript-tutorial-for-react-developers/" target='_blank' rel="noreferrer" >TypeScript Handbook for React Developers - How to Build a Type-Safe Todo App</a>
+      <a href="https://dev.to/lovepreetsingh/react-redux-in-one-shot-while-building-a-project-jbd" target='_blank' rel="noreferrer" >React redux in one shot while building a project</a>
+      <a href="https://dev.to/kelvinguchu/javascript-console-methods-a-deep-dive-jbf" target='_blank' rel="noreferrer" >Javascript console methods a deep dive</a>
+      <a href="https://dev.to/logrocket/understanding-typescript-generators-20lo" target='_blank' rel="noreferrer" >Understanding TypeScript generators</a>
+      <a href="https://blog.openreplay.com/building-a-comment-form-with-react-mentions/" target='_blank' rel="noreferrer">Building a Comment Form with React-Mentions</a>
+      <a href="https://blog.joberty.de/best-github-repositories/" target='_blank' rel="noreferrer">10 Best GitHub Repositories</a>
+      <a href="https://dev.to/documatic/converting-javascript-codebase-to-typescript-1852" target='_blank' rel="noreferrer">Converting javascript codebase to typescript</a>
+      <a href="https://dev.to/gautham495/react-with-redux-the-definitive-guide-4gbj" target='_blank' rel="noreferrer">React with Redux - The Definitive guide</a>
+      <a href="https://dev.to/livecycle/5-react-libraries-to-level-up-your-projects-in-2023-3d5i" target='_blank' rel="noreferrer">5 React Libraries to Level Up your Projects in 2023</a>
+      <a href="https://dev.to/alais29/building-a-real-time-search-filter-in-react-a-step-by-step-guide-3lmm" target='_blank' rel="noreferrer">Building a Real-Time Search Filter in React: A Step-by-Step Guide</a>
+      <a href="https://dev.to/brojenuel/the-best-icon-library-for-web-development-pl0" target='_blank' rel="noreferrer">The Best Icon Library for Web Development</a>
+      <a href="https://react.dev/learn/state-a-components-memory" target='_blank' rel="noreferrer">Spraviť úlohy na spodku stránky</a>
+      <a href="https://zdrojak.cz/clanky/10-veci-jak-ai-pomaha-pri-programovani/" target='_blank' rel="noreferrer">10 veci jak AI pomaha pri programovani</a>
+      <a href="https://touchit.sk/youtube-na-pozadi-aj-bez-platenia-premium/479677" target='_blank' rel="noreferrer">Youtube na pozadi aj bez platenia premium</a>
     </div>
   )
 }
