0989dcdbda32341bd81822e15838efe0514d3c5e
FodoFofo <jozkofodo@gmail.com>
2023-08-12 13:36:12 +0200

    Doplnenie textu a štýlu pre Main

--- src/Views/Main.tsx
+++ src/Views/Main.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Main.scss'
+import React from "react";
+import "./Main.scss";
 
 const Main = () => {
   return (
-    <div className='main'>
-        <p>
-          DOPLNIŤ NEJAKÝ POPIS
-        </p>
-            
-        Main
+    <div className="main">
+      <p>
+        Web apka vytvorená popri učení na precvičovanie preštudovaného
+        materiálu.
+      </p>
+      <h3 className="vlavo">Študijný materiál:</h3>
+      <p className="vlavo odsadenie">&gt; Youtube kanál Davida Šeteka</p>
+      <p className="vlavo odsadenie">&gt; Kurz na Skillmea Od Yablka</p>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
