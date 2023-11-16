f829802732a9e25f7c2f7655b1af21fb14e08fa4
Marek Fodor <fodormarek5@gmail.com>
2023-11-16 22:25:27 +0100

    Vytvor context + provider

--- src/components/Fodo/Recepty/components/RecipeContext.js
+++ src/components/Fodo/Recepty/components/RecipeContext.js
@@ -1,5 +1,44 @@
-import { createContext } from 'react'
+import { useReducer } from "react";
+import { createContext } from "react";
+import { ActionKind, recipeReducer } from "../utilities/RecipeReducer";
 
-const RecipeContext = createContext()
+const RecipeContext = createContext();
 
-export default RecipeContext
+const defaultState = {
+  recipeName: "",
+  ingredientName: "",
+  ingredientQuantity: "",
+  recipeProcedure: "",
+  note: "",
+};
+
+export const RecipeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(recipeReducer, defaultState);
+
+  const addRecipe = (recipe) => {
+    dispatch({
+      type: ActionKind.ADD_RECIPE,
+      payload: recipe,
+    });
+  };
+
+  const changeInput = (name, value) => {
+    dispatch({
+      type: ActionKind.CHANGE_INPUT,
+      payload: {
+        name,
+        value
+      },
+    });
+  };
+
+  const value = {
+    state,
+    changeInput,
+    addRecipe
+  }
+
+  return <RecipeContext.Provider value={value}>{children}</RecipeContext.Provider>;
+};
+
+export default RecipeContext;
