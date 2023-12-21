7ebdbf2c3615af02181242bbbc1db03443734740
FodoFofo <jozkofodo@gmail.com>
2023-12-21 10:40:00 +0100

    upraveie textu na uvodnej stránke

--- src/Views/Main.tsx
+++ src/Views/Main.tsx
@@ -5,12 +5,13 @@ const Main = () => {
   return (
     <div className="main">
       <p>
-        Web apka vytvorená popri učení na precvičovanie preštudovaného
-        materiálu.
+        Web apka vytvorená popri učení na precvičovanie preštudovaného materiálu. Je tu aj pár mojich projektov.
       </p>
       <h3 className="vlavo">Študijný materiál:</h3>
       <p className="vlavo odsadenie">&gt; Youtube kanál Davida Šeteka</p>
       <p className="vlavo odsadenie">&gt; Kurz na Skillmea Od Yablka</p>
+      <p className="vlavo odsadenie">&gt; Youtube</p>
+      <p className="vlavo odsadenie">&gt; a hlavne Google</p>
     </div>
   );
 };
