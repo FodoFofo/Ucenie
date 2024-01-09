104f50cd02c3b701cc5d6d93860cbbc80e91f8ba
FodoFofo <jozkofodo@gmail.com>
2024-01-09 19:43:55 +0100

    Spravenie hracieho poľa

--- src/components/Fodo/BattleShip/AppBattleShip.tsx
+++ src/components/Fodo/BattleShip/AppBattleShip.tsx
@@ -1,15 +1,37 @@
-// štýly
+import { useState } from 'react'
+
+// styles
 import './AppBattleShip.scss'
 
+// data
+import { PlayGroundSize } from './utility/Database'
+
+//components
+import Cell from './components/Cell'
+
 const AppBattleShip = () => {
+    const [cells, setCells] = useState([])
+    const riadky = []
 
-    const param = 'nieco'
+    for (let index = 1; index <= PlayGroundSize.rows; index++) {
+        let bunky = []
 
-    console.log`${param} hello guys `;
-    
-  return (
-    <div>AppBattleShip</div>
-  )
+        for (let index = 1; index <= PlayGroundSize.columns; index++) {
+            bunky.push(<Cell />)
+        }
+
+        riadky.push(<div className={`row${index}`}>{bunky}</div>)
+    }
+
+    return (
+        <div className='app-battle-ship'>
+            <div className="play-ground">
+                <div className="rows">
+                    {riadky}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AppBattleShip
---
+++ src/components/Fodo/BattleShip/components/Cell.tsx
@@ -0,0 +1,11 @@
+//styles
+import './Cell.scss'
+
+const Cell = () => {
+    const nieco = 'boat'
+  return (
+    <div className={`cell ${nieco}`}></div>
+  )
+}
+
+export default Cell
